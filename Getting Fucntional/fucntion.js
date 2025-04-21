a17bc5afb7fc1c6495316d2d56e877867c19e21c
jimcaminoy <caminoyjohnren06@gmail.com>
2025-04-22 00:04:32 +0800

    Update fucntion.js

---
+++ Getting Fucntional/fucntion.js
@@ -0,0 +1,43 @@
+
+ var dogName = "skibidi";
+ var dogWeight = 23;
+ if (dogWeight > 20) {
+     console.log(dogName + " says WOOF WOOF");
+ } else {
+     console.log(dogName + " says woof woof");
+ }
+
+ var dogName = "jent";
+ var dogWeight = 15;
+ if (dogWeight > 20) {
+     console.log(dogName + " says WOOF WOOF");
+ } else {
+     console.log(dogName + " says woof woof");
+ }
+
+ var dogName = "steven";
+ var dogWeight = 50;
+ if (dogWeight > 20) {
+     console.log(dogName + " says WOOF WOOF");
+ } else {
+     console.log(dogName + " says woof woof");
+ }
+
+ var dogName = "rheymar";
+ var dogWeight = 10;
+ if (dogWeight > 20) {
+     console.log(dogName + " says WOOF WOOF");
+ } else {
+     console.log(dogName + " says woof woof");
+ }
+
+function bark (dogName, dogWeight) {
+    if (dogWeight > 20) {
+        console.log(dogName + " says WOOF WOOF");
+    } else {
+        console.log(dogName + " says woof woof");
+    }
+}
+
+bark("jent", 20)
+bark("rheymar", 100)
